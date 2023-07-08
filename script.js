859361458ce126046fb6e468100aea71c0be2d4f
SystemJargon <24641464+SystemJargon@users.noreply.github.com>
2023-07-08 12:20:42 +1200

    Fix years variable

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.getElementById('interestForm').addEventListener('submit', function(even
   event.preventDefault(); // Prevent form submission
   var balance = parseFloat(document.getElementById('balance').value);
   var interestRate = parseFloat(document.getElementById('interestRate').value);
-  var years = 1; // You can change the number of years to calculate here
+  var years = parseInt(document.getElementById('years').value);
+  
   var rateOfReturn = balance * (Math.pow((1 + (interestRate / 100)), years)) - balance;
-
+  
   document.getElementById('result').textContent = 'Rate of Return: $' + rateOfReturn.toFixed(2);
 });
