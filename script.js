22ff192f14664835862b789b1ca520fe083ad945
SystemJargon <24641464+SystemJargon@users.noreply.github.com>
2023-07-08 12:15:43 +1200

    Update script.js
    Var years set to 1 year. But can be changed.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('interestForm').addEventListener('submit', function(even
   event.preventDefault(); // Prevent form submission
   var balance = parseFloat(document.getElementById('balance').value);
   var interestRate = parseFloat(document.getElementById('interestRate').value);
-  var years = 5; // You can change the number of years to calculate here
+  var years = 1; // You can change the number of years to calculate here
   var rateOfReturn = balance * (Math.pow((1 + (interestRate / 100)), years)) - balance;
 
   document.getElementById('result').textContent = 'Rate of Return: $' + rateOfReturn.toFixed(2);
